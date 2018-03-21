47deb080954cf9c010ba3ba92812968964d70aea
krikchaip <krikchai.p@gmail.com>
2018-03-21 22:23:58 +0700

    Dynamic Imports

--- src/index.js
+++ src/index.js
@@ -1,13 +1,15 @@
-import _ from 'lodash';
-
-function component() {
-  var element = document.createElement('div');
-
-  // Lodash, initially included via a script, is required for this line to work
-  // now imported by this script
-  element.innerHTML = _.join(['Hello', 'webpack'], ' ');
-
-  return element;
+function getComponent() {
+  // Note the use of webpackChunkName in the comment.
+  // This will cause our separate bundle to be named lodash.bundle.js instead of just [id].bundle.js.
+  return import(/* webpackChunkName: "lodash" */ 'lodash')
+    .then(({ default: _ }) => {
+      var element = document.createElement('div');
+      element.innerHTML = _.join(['Hello', 'webpack'], ' ');
+      return element;
+    })
+    .catch(error => 'An error occurred while loading the component');
 }
 
-document.body.appendChild(component());
+getComponent().then(component => {
+  document.body.appendChild(component);
+})
--- webpack.config.js
+++ webpack.config.js
@@ -4,17 +4,15 @@ const HTMLWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
   entry: {
-    // lodash is also imported within ./src/another-module.js and will thus be duplicated in both bundles.
     index: './src/index.js',
-    another: './src/another-module.js'
   },
   output: {
     filename: '[name].bundle.js',
+    chunkFilename: '[name].bundle.js', // visit: https://webpack.js.org/configuration/output/#output-chunkfilename
     path: path.resolve(__dirname, 'dist')
   },
   plugins: [
     new HTMLWebpackPlugin({ title: 'Code Splitting' }),
-    new webpack.optimize.CommonsChunkPlugin({ name: 'common-bundle-name' }), // DEPRECATED
   ],
 };
 
