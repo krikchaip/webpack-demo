f022ae6588d9e796ee3b6fe4cc89b789339a96ff
r3nniw <r3nniw@r3nniws-MacBook-Pro.local>
2018-03-21 15:13:50 +0700

    Loading Fonts

--- webpack.config.js
+++ webpack.config.js
@@ -10,6 +10,7 @@ module.exports = {
     rules: [
       { test: /\.css$/, use: ['style-loader', 'css-loader'] },
       { test: /\.(png|svg|jpg|gif)$/, use: ['file-loader'] },
+      { test: /\.(woff|woff2|eot|ttf|otf)$/, use: ['file-loader'] }
     ]
   }
 };
