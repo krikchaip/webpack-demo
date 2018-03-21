334eb2423bf20367725f3640195c61239c9ce93e
r3nniw <krikchai.p@gmail.com>
2018-03-21 17:12:43 +0700

    Hot Module Replacement

--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 import _ from 'lodash';
 import printMe from './print';
+import './style.css';
 
 function component() {
   var element = document.createElement('div');
@@ -17,4 +18,23 @@ function component() {
   return element;
 }
 
-document.body.appendChild(component());
+// Store the element to re-render on print.js changes
+let element = component();
+
+// document.body.appendChild(component());
+document.body.appendChild(element);
+
+// if enable hot reload
+if (module.hot) {
+  // when a change inside print.js is detected we tell webpack to accept the updated module.
+  module.hot.accept('./print.js', function() {
+    console.log('Accepting the updated printMe module!');
+
+    // this changed. but the button's onclick event handler is still bound to the original printMe function.
+    printMe();
+
+    document.body.removeChild(element);
+    element = component(); // Re-render the "component" to update the click handler
+    document.body.appendChild(element);
+  })
+}
--- webpack.config.js
+++ webpack.config.js
@@ -1,25 +1,27 @@
 const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 const CleanWebpackPlugin = require('clean-webpack-plugin');
+const webpack = require('webpack');
 
 module.exports = {
   devtool: 'inline-source-map', // visit: https://webpack.js.org/configuration/devtool/
-  devServer: { contentBase: './dist' },
-  entry: {
-    app: './src/index.js',
-    print: './src/print.js',
-  },
+  devServer: { contentBase: './dist', hot: true },
+  entry: { app: './src/index.js' },
   output: {
     filename: '[name].bundle.js', // dynamically generate bundle names, based on the entry point names
     path: path.resolve(__dirname, 'dist')
   },
   module: {
     rules: [
+      // This loader uses module.hot.accept behind the scenes to patch <style> tags when CSS dependencies are updated.
+      { test: /\.css$/, use: ['style-loader', 'css-loader'] },
     ]
   },
   plugins: [
-    new HtmlWebpackPlugin({ title: 'Output Management' }), // automatically manages HTML output file
+    new HtmlWebpackPlugin({ title: 'Hot Module Replacement' }), // automatically manages HTML output file
     new CleanWebpackPlugin(['dist']), // automatically clean 'dist' folder before building
+    new webpack.HotModuleReplacementPlugin(), // enable HMR
+    new webpack.NamedModulesPlugin(), // make it easier to see which dependencies are being patched
   ]
 };
 
