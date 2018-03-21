d155b5157c4968c0a81f5886a20d75b42093d83a
krikchaip <krikchai.p@gmail.com>
2018-03-21 18:26:19 +0700

    Tree-Shaking (Deadcode elimination)

--- src/index.js
+++ src/index.js
@@ -1,11 +1,20 @@
+import { cube } from './math';
 import _ from 'lodash';
+import './style.css';
+
+// Note that we did not import the square method from the src/math.js module.
+// That function is what's known as "dead code", meaning an unused export that should be dropped.
+
+// we use "sideEffects" property to provide hints to webpack's compiler on the "pureness" of your code.
+// visit: https://webpack.js.org/guides/tree-shaking/
 
 function component() {
-  var element = document.createElement('div');
+  var element = document.createElement('pre');
 
-  // Lodash, initially included via a script, is required for this line to work
-  // now imported by this script
-  element.innerHTML = _.join(['Hello', 'webpack'], ' ');
+  element.innerHTML = [
+    'Hello webpack!',
+    '5 cubed is equal to ' + cube(5)
+  ].join('\n\n');
 
   return element;
 }
--- webpack.config.js
+++ webpack.config.js
@@ -1,10 +1,20 @@
 const path = require('path');
 
 module.exports = {
+  mode: 'production', // dead-code will be removed on 'production'
   entry: './src/index.js',
   output: {
     filename: 'bundle.js',
     path: path.resolve(__dirname, 'dist')
+  },
+  module: {
+    rules: [
+      // side-effect by default(not eliminated)
+      { test: /\.css$/, use: ['style-loader', 'css-loader'] },
+
+      // modules are side-effect by default unless specified
+      { include: path.resolve("node_modules", "lodash"), sideEffects: false },
+    ]
   }
 };
 
