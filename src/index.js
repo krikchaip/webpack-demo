6b4a76bcf94d7480aa0b0cedb91f315cdad5ce67
r3nniw <r3nniw@r3nniws-MacBook-Pro.local>
2018-03-21 15:42:12 +0700

    Preparation

--- src/index.js
+++ src/index.js
@@ -1,23 +1,18 @@
 import _ from 'lodash';
-import './style.css';
-import Icon from './icon.jpg';
-import Data from './data.xml'; // able to import JSON without any loader
+import printMe from './print';
 
 function component() {
   var element = document.createElement('div');
+  var btn = document.createElement('button');
 
   // Lodash, initially included via a script, is required for this line to work
   // now imported by this script
   element.innerHTML = _.join(['Hello', 'webpack'], ' ');
-  element.classList.add('hello');
 
-  // Add the image to our existing div.
-  var myIcon = new Image();
-  myIcon.src = Icon;
+  btn.innerHTML = 'Click me and check the console!';
+  btn.onclick = printMe;
   
-  element.appendChild(myIcon);
-
-  console.log(Data);
+  element.appendChild(btn);
 
   return element;
 }
--- webpack.config.js
+++ webpack.config.js
@@ -1,18 +1,16 @@
 const path = require('path');
 
 module.exports = {
-  entry: './src/index.js',
+  entry: {
+    app: './src/index.js',
+    print: './src/print.js',
+  },
   output: {
-    filename: 'bundle.js',
+    filename: '[name].bundle.js', // dynamically generate bundle names, based on the entry point names
     path: path.resolve(__dirname, 'dist')
   },
   module: {
     rules: [
-      { test: /\.css$/, use: ['style-loader', 'css-loader'] },
-      { test: /\.(png|svg|jpg|gif)$/, use: ['file-loader'] },
-      { test: /\.(woff|woff2|eot|ttf|otf)$/, use: ['file-loader'] },
-      { test: /\.(csv|tsv)$/, use: ['csv-loader'] },
-      { test: /\.xml$/, use: ['xml-loader'] },
     ]
   }
 };
