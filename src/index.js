af7debecf3a896912e2dd6569bdf33ecdf2f05e2
r3nniw <r3nniw@r3nniws-MacBook-Pro.local>
2018-03-21 15:24:03 +0700

    Loading Data

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 import _ from 'lodash';
 import './style.css';
 import Icon from './icon.jpg';
+import Data from './data.xml'; // able to import JSON without any loader
 
 function component() {
   var element = document.createElement('div');
@@ -15,6 +16,8 @@ function component() {
   myIcon.src = Icon;
   
   element.appendChild(myIcon);
+
+  console.log(Data);
 
   return element;
 }
